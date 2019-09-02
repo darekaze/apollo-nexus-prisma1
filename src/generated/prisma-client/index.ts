cd1d3552f2ec7c625c865542c48c39e9682c3dec
darekaze <darekaze@gmail.com>
2019-09-02 12:26:30 +0900

    Change name to endpoint

--- src/generated/prisma-client/index.ts
+++ src/generated/prisma-client/index.ts
@@ -2046,7 +2046,7 @@ export const models: Model[] = [
 export const Prisma = makePrismaClientClass<ClientConstructor<Prisma>>({
   typeDefs,
   models,
-  endpoint: `http://${process.env["PRISMA_API"]}:4466/${
+  endpoint: `http://${process.env["PRISMA_ENDPOINT"]}:4466/${
     process.env["DB_NAME"]
   }/${process.env["DB_STAGE"]}`
 });
